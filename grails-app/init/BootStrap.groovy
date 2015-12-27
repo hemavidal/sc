2cc66844d06bb4248a9f1fbc16443f815b5dfba7
Hemã Vidal <hemavidal@gmail.com>
2015-12-26 22:39:52 -0300

    - Removendo Filtro de Igreja que estava dando problema. - Removendo script de recuperação do BD antigo para testar se o deploy funciona.

--- grails-app/init/BootStrap.groovy
+++ grails-app/init/BootStrap.groovy
@@ -5,7 +5,7 @@ class BootStrap {
 	def importCSVService
 
     def init = { servletContext ->
-        importCSVService.importCampinaGrande(servletContext.getRealPath(""))
+        //importCSVService.importCampinaGrande(servletContext.getRealPath(""))
     	
     	new Usuario(login: "admin", senha: "126366", tipo:"admin").save(flush:true)
 
